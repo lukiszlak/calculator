bafae73bb0754fcc14f4cfdbf093bf3f40c13d18
Szlachu <48260901+lukiszlak@users.noreply.github.com>
2025-11-11 08:15:04 +0100

    Getting rid of debug prints and moving logic for managing the operation out of main function for easier test handling

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,33 +3,28 @@ package org.example
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main(args: Array<String>) {
-    println("Arguments = ${args.joinToString("")}")
     val operation = args.joinToString("")
-    println("operation is $operation")
 
+    val operationResults = manageOperation(operation)
+    println(operationResults)
+}
+
+fun manageOperation(operation: String): Int {
     val operator = getOperation(operation)
-    println("Operator is $operator")
-    println("Getting Values")
     val values = operation.split(operator)
-    println("values are $values ")
-    println("Getting Operation Results")
     val operationResult = runOperation(operator, values)
-    println(operationResult)
-
+    return operationResult
 }
 
 fun getOperation(operation: String): String {
-    println("Getting Operator")
     val regexExpression = "\\d+(.*?)\\d+".toRegex()
     val matchResults = regexExpression.find(operation)
 
     val operator = matchResults!!.groups[1]!!.value
-    println("Operator is $operator")
     return operator
 }
 
 fun runOperation(operator: String, values: List<String>): Int {
-    println("Getting Run Operation from $values")
     return when(operator) {
         "+"-> values[0].toInt() + values[1].toInt()
         "-"-> values[0].toInt() - values[1].toInt()
